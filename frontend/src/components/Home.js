7aef6729107f2be4e98ceeac65b97214637d9a9e
pratham4121996 <121903974+pratham4121996@users.noreply.github.com>
2025-02-23 18:11:46 -0600

    Home file ui

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,44 @@
+import Homepage from "./routes/home.js"; //importing homepage
+import React , {useState} from "react";
+import axios from "axios";
+
+const Home = () => {
+
+//state constants can be edited
+    const[YoutubeLink , SetYoutubeLink] = useState(""); //to store link
+    const[Transcript , SetTranscript] = useState(""); // to store transcript
+    const[Quiz , SetQuiz] = useState("");//to store quiz
+
+
+    const submitRequest = () =>{
+      //handle api requests
+    }
+
+    return(
+
+        <div>
+            <h1>Quiz Generator</h1>
+            <input
+                type = "text"
+                placeholder = "Enter the Link"
+                value = {youtubeLink}
+                onChange = {(e) => SetYoutubeLink(e.target.value)}
+            />
+
+            <button onClick = {submitRequest}>Generate Quiz</button>
+
+            //display the quiz
+            {quiz && (
+                 <div>
+                     <h2>Generated Quiz:</h2>
+                     <p>{quiz}</p>
+                 </div>
+                    )}
+
+        </div>
+
+
+    );
+};
+
+export default Home;
