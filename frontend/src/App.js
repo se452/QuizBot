c1dce8becf423404eecdec6f1849e50532a0b8d1
Yogesh lakhani <yogeshlakhani@Yogeshs-MacBook-Air-2.local>
2025-02-27 00:17:44 -0600

    final push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./routes/login";
 import Home from "./routes/home";
 import Signup from "./routes/signup";
@@ -9,9 +9,12 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
+        {/* Redirect any unknown routes to home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
   );
